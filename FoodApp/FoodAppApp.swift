357fd49a6d838e928ad79e2f33460c32c28e69a7
AprilGreen42 <aprilgreen042@gmail.com>
2024-08-08 02:46:07 +0400

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Дмитрий on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
